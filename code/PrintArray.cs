470e55b13fbdf5be2f6f1b0fe898bef7561ca7b9
tobikareem <toboibo@yahoo.com>
2025-02-28 18:17:56 -0800

    Contribute to Lawrence code

---
+++ code/PrintArray.cs
@@ -0,0 +1,9 @@
+
+namespace LearnGit;
+
+public class PrintArrayItems{
+    public static int Count(int[] arr)
+    {
+        return arr.Length;
+    }
+}
--- code/Program.cs
+++ code/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using LearnGit;
+
+int result = PrintArrayItems.Count(new []{4,5, 6,7});
+
+Console.WriteLine($"The count of the array items is {result}");
